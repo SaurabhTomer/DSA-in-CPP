e49835e5e7184a7c05bc9b7b01a537226ec1100a
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-08-03 00:12:44 +0530

    unordered

--- Learn basics/hashing/unordered_hasing.cpp
+++ Learn basics/hashing/unordered_hasing.cpp
@@ -1,40 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cout << "Size of array: ";
     cin >> n;
     int arr[n];
 
-
-
     cout << "Enter elements:\n";
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
-            
     }
 
     // pre computee
-    map<int,int> mpp;       // store value in sort manner
-    for(int i = 0; i < n; i++) {
+    unordered_map<int, int> mpp; // store value in sort manner
+    for (int i = 0; i < n; i++)
+    {
         mpp[arr[i]]++;
-            
     }
-//iterate in map
-    for(auto it : mpp){
-        cout << it.first<<"->" << it.second << endl;
+    // iterate in map
+    for (auto it : mpp)
+    {
+        cout << it.first << "->" << it.second << endl;
     }
-    
 
     int q;
     cout << "Enter number of queries: ";
     cin >> q;
 
-    while(q--) {
+    while (q--)
+    {
         int num;
         cin >> num;
-        //fetch
+        // fetch
         cout << num << " appears " << mpp[num] << " times\n";
     }
 
